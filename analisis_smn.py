df05896464687f89f9a28e7d1d81c3c88ae54d31
Joaquin Diaz <dgjoaco@gmail.com>
2026-09-14 19:39:28 -0300

    agregue función separar_viento

---
+++ analisis_smn.py
@@ -0,0 +1,14 @@
+def separar_viento(campo_viento: str) -> tuple:
+    partes = campo_viento.split()
+    if len(partes) == 1:
+        direccion = partes[0]
+        velocidad = 0
+    else:
+        direccion = partes[0]
+        velocidad = float(partes[1])
+
+    return direccion, velocidad
+
+if __name__ == "__main__":
+    print(separar_viento("Norte  5"))
+    print(separar_viento("Calma"))
